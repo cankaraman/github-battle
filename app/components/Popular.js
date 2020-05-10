0089b04138b718a29eef3db60d16843857826be4
can karaman <ycankaraman@gmail.com>
2020-05-11 01:22:08 +0300

    refactor to add pure function component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+function LanguagesNav({ selectedLanguage, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="flex-center">
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            style={language === selectedLanguage
+              ? { color: 'rgb(187, 46, 31)' }
+              : null}
+            className="btn-clear nav-link"
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);
@@ -18,23 +40,14 @@ export default class Popular extends React.Component {
   }
 
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const { selectedLanguage } = this.state;
     return (
-      <ul className="flex-center">
-        {languages.map((language) => (
-          <li key={language}>
-            <button
-              style={language === this.state.selectedLanguage
-                ? { color: 'rgb(187, 46, 31)' }
-                : null}
-              className="btn-clear nav-link"
-              onClick={() => this.updateLanguage(language)}
-            >
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <LanguagesNav
+          selectedLanguage={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </div>
     );
   }
 }

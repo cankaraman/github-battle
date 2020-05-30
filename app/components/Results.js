2e8fa2a596d91fb0f2f210bea05372a0edf489cb
can karaman <ycankaraman@gmail.com>
2020-05-30 15:09:59 +0300

    add results ui

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser,
+} from 'react-icons/fa';
 import { battle } from '../utils/api';
 
+function Profile({ player, winStatus }) {
+  return (
+    <div className="card bg-light">
+      <h4 className="header-lg center-text">
+        {winStatus}
+      </h4>
+      <img
+        className="avatar"
+        src={player.profile.avatar_url}
+        alt={`Avatar for ${player.profile.login}`}
+      />
+      <h4 className="center-text">
+        Score:
+        {' '}
+        {player.score.toLocaleString()}
+      </h4>
+      <h2 className="center-text">
+        <a className="link" href={player.profile.html_url}>
+          {player.profile.login}
+        </a>
+      </h2>
+      <ul className="card-list">
+        <li>
+          <FaUser color="rgb(239, 155, 155)" size={22} />
+          {player.profile.name}
+        </li>
+        {player.profile.location && (
+          <li>
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
+              {player.profile.location}
+          </li>
+        )}
+        {player.profile.company && (
+        <li>
+          <FaBriefcase color="#795548" size={22} />
+          {player.profile.company}
+        </li>
+        )}
+        <li>
+          <FaUsers color="rgb(129, 195, 245)" size={22} />
+          {player.profile.followers.toLocaleString()}
+          {' '}
+          followers
+        </li>
+        <li>
+          <FaUserFriends color="rgb(64, 183, 95)" size={22} />
+          {player.profile.following.toLocaleString()}
+          {' '}
+          following
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  player: PropTypes.object.isRequired,
+  winStatus: PropTypes.string.isRequired,
+};
 
 export default class Results extends React.Component {
   constructor(props) {
@@ -10,7 +73,7 @@ export default class Results extends React.Component {
       winner: null,
       loser: null,
       error: null,
-      loading: null,
+      loading: true,
     };
   }
 
@@ -24,7 +87,6 @@ export default class Results extends React.Component {
           error: null,
           loading: false,
         });
-        console.log('data: ', players);
       }).catch(({ message }) => {
         this.setState({
           error: message,
@@ -34,10 +96,22 @@ export default class Results extends React.Component {
   }
 
   render() {
+    const {
+      winner, loser, error, loading,
+    } = this.state;
+
+    if (loading) {
+      return <p>LOADING</p>;
+    }
+
+    if (error) {
+      return <p className="center-text error">{error}</p>;
+    }
+
     return (
-      <div>
-        Results
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      <div className="grid space-around container-sm">
+        <Profile player={winner} winStatus={winner.score === loser.score ? 'Tie' : 'Winner'} />
+        <Profile player={loser} winStatus={winner.score === loser.score ? 'Tie' : 'Loser'} />
       </div>
     );
   }

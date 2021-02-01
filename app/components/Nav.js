28103d50d416230d42da32f274dd185cea817a48
cankaraman <ycankaraman@gmail.com>
2021-02-01 14:43:34 +0300

    Refactor Nav to useContext

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,48 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { ThemeConsumer } from "../contexts/theme";
+import themeContext from "../contexts/theme";
 
 const activeStyle = {
   color: "rgb(187, 46, 31)",
 };
 
 export default function Nav() {
+  const { theme, toggleTheme } = useContext(themeContext);
+
   return (
-    <ThemeConsumer>
-      {({ theme, toggleTheme }) => {
-        return (
-          <nav className="row space-between">
-            <ul className="row nav">
-              <li>
-                <NavLink
-                  exact
-                  activeStyle={activeStyle}
-                  to="/"
-                  className="nav-link"
-                >
-                  Popular
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  activeStyle={activeStyle}
-                  to="/battle"
-                  className="nav-link"
-                >
-                  Battle
-                </NavLink>
-              </li>
-            </ul>
-            <button
-              style={{ fontSize: 30 }}
-              className="btn-clear"
-              onClick={toggleTheme}
-            >
-              {theme === "light" ? "🔦" : "💡"}
-            </button>
-          </nav>
-        );
-      }}
-    </ThemeConsumer>
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink exact activeStyle={activeStyle} to="/" className="nav-link">
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={activeStyle} to="/battle" className="nav-link">
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button
+        style={{ fontSize: 30 }}
+        className="btn-clear"
+        onClick={toggleTheme}
+      >
+        {theme === "light" ? "🔦" : "💡"}
+      </button>
+    </nav>
   );
 }
